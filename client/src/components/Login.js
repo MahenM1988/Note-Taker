06691160ebcb3c0597912c372ba74be6e9225f53
Mahen Mahindaratne <mmahindaratne88@gmail.com>
2024-12-13 21:14:06 +0530

    Update Login.js

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import '../App.css';  // Import the updated CSS
+import '../App.css'; 
 
 const Login = ({ handleLogin }) => {
   const [username, setUsername] = useState('');
